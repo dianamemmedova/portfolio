3e578efda504ef16148c5cb6436f924070b64fca
dianamemmedova <dianamemmedova653@gmeil.com>
2026-06-16 16:38:24 +0400

    feat: initial project setup and global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeroComponent } from './features/hero/hero.component';
+import { AboutComponent } from './features/about/about.component';
+import { SkillsComponent } from './features/skills/skills.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { GithubComponent } from './features/github/github.component';
+import { ContactComponent } from './features/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [ NavbarComponent, FooterComponent, HeroComponent, AboutComponent, SkillsComponent, ProjectsComponent, GithubComponent,  ContactComponent],
+  template: `
+    <app-navbar />
+    <main>
+      <app-hero />
+      <app-about />
+      <app-skills />
+      <app-projects />
+      <app-github />
+      <app-contact />
+    </main>
+    <app-footer />
+  `,
 })
-export class AppComponent {
-  title = 'portfolio';
-}
+export class AppComponent {}
